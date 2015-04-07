cbaff55d4b464d730ee89fcd491df373d5ae343d
Leonardo Kaplan <leokaplan@users.noreply.github.com>
2015-04-07 12:07:51 -0300

    Update table.c

--- minix/servers/pm/table.c
+++ minix/servers/pm/table.c
@@ -58,4 +58,8 @@ int (* const call_vec[NR_PM_CALLS])(void) = {
 	CALL(PM_GETEPINFO)	= do_getepinfo,		/* getepinfo(2) */
 	CALL(PM_GETPROCNR)	= do_getprocnr,		/* getprocnr(2) */
 	CALL(PM_GETSYSINFO)	= do_getsysinfo		/* getsysinfo(2) */
+	CALL(PM_SENDMSG)	= do_MQsend,
+ 	CALL(PM_RECVMSG)	= do_MQrecv,	
+ 	CALL(PM_COUNTMSG)	= do_MQcount,	
+ 	CALL(PM_INSPECTMSG)	= do_MQinspect
 };
